b4b4fc5c2cf4264bf0f355f339a831402058971b
DingAkiJin <jin.din@husky.neu.edu>
2017-11-09 20:22:04 -0500

    assignment 5

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,19 +1,6 @@
-// var mongoose = require("mongoose");
-// var db = mongoose.connect('mongodb://localhost/cs5610',{
-//   useMongoClient:true
-// });
-// module.exports = db;
-module.exports  = function () {
+var mongoose = require("mongoose");
+var db = mongoose.connect('mongodb://localhost/cs5610',{
+  useMongoClient:true
+});
+module.exports = db;
 
-
-
-
-  var models = {
-    UserModel: require("./user/user.model.server")(),
-    WebsiteModel: require("./website/website.model.server")(),
-    PageModel: require("./page/page.model.server")(),
-    WidgetModel: require("./widget/widget.model.server")()
-    //TODO : add all other models
-  };
-  return models;
-};
