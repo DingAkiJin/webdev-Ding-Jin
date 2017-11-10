f2209a03740031ab4553ced2e963a836f8a6e421
DingAkiJin <jin.din@husky.neu.edu>
2017-11-09 20:11:25 -0500

    fix mongod

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,5 +1,19 @@
-var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/cs5610',{
-  useMongoClient:true
-});
-module.exports = db;
+// var mongoose = require("mongoose");
+// var db = mongoose.connect('mongodb://localhost/cs5610',{
+//   useMongoClient:true
+// });
+// module.exports = db;
+module.exports  = function () {
+
+
+
+
+  var models = {
+    UserModel: require("./user/user.model.server")(),
+    WebsiteModel: require("./website/website.model.server")(),
+    PageModel: require("./page/page.model.server")(),
+    WidgetModel: require("./widget/widget.model.server")()
+    //TODO : add all other models
+  };
+  return models;
+};
